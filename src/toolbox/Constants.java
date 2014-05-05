ab3896b8b01e484e4319f68e2074302a1c09cbbd
paul <paul@quant>
2014-05-05 16:58:55 -0400

    added WEB_PAGE_LOG_FORMAT

--- src/toolbox/Constants.java
+++ src/toolbox/Constants.java
@@ -14,6 +14,7 @@ public class Constants {
     public static final Level DEFAULT_LOG_LEVEL = Level.INFO;
     public static final String DEFAULT_LOG_FORMAT = "%m%n";
     public static final String SAME_LINE_LOG_FORMAT = "%m";     //no carriage return, so the next logging statement is on the same line
+    public static final String WEB_PAGE_LOG_FORMAT = "<br>%m%n";
     public static final String DEFAULT_SEPARATOR = ",";
     public static final String UNKNOWN = "unknown";
 }
