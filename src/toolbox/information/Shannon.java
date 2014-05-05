42f1fc24c95f41ae6aa9e965cf054517f70c9d52
paul <paul@quant>
2014-05-05 17:02:11 -0400

    made Shannon generic and added getEntropyRatio

--- src/toolbox/information/Shannon.java
+++ src/toolbox/information/Shannon.java
@@ -32,34 +32,48 @@ public class Shannon {
         return list.getEntropy();
     }
     
-    public static double getEntropy(List<Double> input) {
+    public static <T> double getEntropy(List<T> input) {
         return new Histogram(input).getEntropy();
     }
     
-    public static double getMutualInformation(List<Double> left, List<Double> right) {
+    public static <T> double getMutualInformation(List<T> left, List<T> right) {
         if(left == null || right == null || left.size() != right.size()) {
             return -1.0;
         }
         
-        List<List<Double>> combined = getCombinedList(left, right);
+        List<List<T>> combined = getCombinedList(left, right);
         double HL = new Histogram(left).getEntropy();
         logger.debug("HL = " + HL);
         double HR = new Histogram(right).getEntropy();
         logger.debug("HR = " + HR);
         double HLR = new Histogram(combined).getEntropy();
-        //new Histogram(combined).display();
         logger.debug("HLR = " + HLR);
         return HL + HR - HLR;
     }
     
-    protected static List<List<Double>> getCombinedList(List<Double> left, List<Double> right) {
+    public static <T> double getEntropyRatio(List<T> left, List<T> right) {
         if(left == null || right == null || left.size() != right.size()) {
-            return new ArrayList<List<Double>>();
+            return -1.0;
         }
-        ArrayList<List<Double>> combined = new ArrayList<List<Double>>();
-        ArrayList<Double> current = new ArrayList<Double>();
+        
+        List<List<T>> combined = getCombinedList(left, right);
+        double HL = new Histogram(left).getEntropy();
+        logger.debug("HL = " + HL);
+        double HR = new Histogram(right).getEntropy();
+        logger.debug("HR = " + HR);
+        double HLR = new Histogram(combined).getEntropy();
+        logger.debug("HLR = " + HLR);
+        return HLR / (HL + HR);     //TODO:  deal with case where HL + HR = 0
+    }
+    
+    protected static <T> List<List<T>> getCombinedList(List<T> left, List<T> right) {
+        if(left == null || right == null || left.size() != right.size()) {
+            return new ArrayList<List<T>>();
+        }
+        ArrayList<List<T>> combined = new ArrayList<List<T>>();
+        ArrayList<T> current = new ArrayList<T>();
         for(int i = 0; i < left.size(); i++) {
-            current = new ArrayList<Double>();
+            current = new ArrayList<T>();
             current.add(left.get(i));
             current.add(right.get(i));
             combined.add(current);
