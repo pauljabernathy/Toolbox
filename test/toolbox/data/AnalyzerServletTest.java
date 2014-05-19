070d5b178244fdf642fbc9b695d57c95860daa70
paul <paul@quant>
2014-05-19 15:11:52 -0400

    added AnalyzerServletTest and AnalyzerTest

---
+++ test/toolbox/data/AnalyzerServletTest.java
@@ -0,0 +1,140 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toolbox.data;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.apache.log4j.*;
+import toolbox.Utilities;
+import java.util.regex.*;
+
+/**
+ *
+ * @author paul
+ */
+public class AnalyzerServletTest {
+    
+    private static Logger logger;
+    
+    public AnalyzerServletTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        logger = Utilities.getLogger(AnalyzerServletTest.class, Level.DEBUG);
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testDoGet() throws Exception {
+    }
+
+    @Test
+    public void testGetColumns() {
+        logger.info("\ntesting getColumns()");
+        AnalyzerServlet instance = new AnalyzerServlet();
+        int[] result = null;
+        try {
+            instance.getColumns(null);
+            fail("Should have thrown the exception but did not");
+        } catch(Exception e) {
+            assertEquals(AnalyzerServlet.ENTER_COLUMNS_MESSAGE, e.getMessage());
+        }
+        
+        try {
+            instance.getColumns("");
+            fail("Should have thrown the exception but did not");
+        } catch(Exception e) {
+            assertEquals(AnalyzerServlet.ENTER_COLUMNS_MESSAGE, e.getMessage());
+        }
+        
+        try {
+            instance.getColumns(",");
+            fail("Should have thrown the exception but did not");
+        } catch(Exception e) {
+            assertEquals(AnalyzerServlet.INVALID_COLUMNS_FORMAT_MESSAGE, e.getMessage());
+        }
+        Pattern p = Pattern.compile("[^\\d^,]");
+        Matcher m = p.matcher("12fd 8,");
+        m = p.matcher("1,2");
+        while(m.find()) {
+            logger.debug(m.start());
+        }
+        logger.debug("---");
+        try {
+            instance.getColumns("1 2");
+            fail("Should have thrown the exception but did not");
+        } catch(Exception e) {
+            assertEquals(AnalyzerServlet.INVALID_COLUMNS_FORMAT_MESSAGE, e.getMessage());
+        }
+        
+        String s = "2";
+        String[] ss = s.split(",");
+        //logger.debug("ss.length = " + ss.length);
+        for(int i = 0; i < ss.length; i++) {
+            //logger.debug(ss[i]);
+        }
+        try {
+            result = instance.getColumns(",2");
+            //A comma at the beginning should work.
+            //fail("Should have thrown an exception for \",2\" but did not");
+            assertEquals(1, result.length);
+            logger.debug(",2 -> " + Utilities.arrayToString(result));
+        } catch(Exception e) {
+            logger.error(e.getClass() + " " + e.getMessage());
+            assertEquals(AnalyzerServlet.INVALID_COLUMNS_FORMAT_MESSAGE, e.getMessage());
+        }
+        
+        try {
+            result = instance.getColumns("1,");
+            //It is OK if there is a trailing comma.
+            assertEquals(1, result.length);
+            logger.debug("1, -> " + Utilities.arrayToString(result));
+        } catch(Exception e) {
+            logger.error(e.getClass() + " " + e.getMessage());
+        }
+        
+        try {
+            result = instance.getColumns("1");
+            //It is OK if there is a trailing comma.
+            assertEquals(1, result.length);
+            logger.debug("1 -> " + Utilities.arrayToString(result));
+        } catch(Exception e) {
+            logger.error(e.getClass() + " " + e.getMessage());
+        }
+        
+        try {
+            result = instance.getColumns("1,2");
+            assertEquals(2, result.length);
+            logger.debug("1,2 -> " + Utilities.arrayToString(result));
+        } catch(Exception e) {
+            fail("input was valid (1,2) but threw exception with message: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testDoPost() throws Exception {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+}
---
+++ test/toolbox/data/AnalyzerTest.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toolbox.data;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.apache.log4j.*;
+import toolbox.Utilities;
+
+/**
+ *
+ * @author paul
+ */
+public class AnalyzerTest {
+    
+    private static Logger logger;
+    
+    public AnalyzerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        logger = Utilities.getLogger(AnalyzerTest.class, Level.DEBUG);
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+
+    @Test
+    public void testAddWriterAppender() {
+    }
+
+    @Test
+    public void testAnalyzeFile_String_String() {
+    }
+
+    @Test
+    public void testAnalyzeFile_3args() {
+    }
+
+    @Test
+    public void testGetEndLine() {
+    }
+
+    @Test
+    public void testSetEndLine() {
+    }
+    
+    @Test
+    public void testHasChanged() {
+        logger.info("\ntesting hasChanged()");
+        Analyzer instance = new Analyzer();
+        logger.debug(instance.hasChanged());
+        instance.notifyObservers();
+        logger.debug(instance.hasChanged());
+    }
+}
