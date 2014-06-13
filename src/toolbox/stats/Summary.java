7b7264b6130abacc2b2133fc338c916e26ac3d8f
paul <paul@quant>
2014-06-13 16:50:30 -0400

    added toString() to Summary

--- src/toolbox/stats/Summary.java
+++ src/toolbox/stats/Summary.java
@@ -17,4 +17,13 @@ public class Summary {
     public double thirdQ;
     public double max;
     public double sd;
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        String sep = "\t";
+        sb.append("min").append(sep).append("1st Q").append(sep).append("median").append(sep).append("mean").append(sep).append("3rd Q").append(sep).append("max").append(sep).append("sd");
+        sb.append("\n");
+        sb.append(min).append(sep).append(firstQ).append(sep).append(median).append(sep).append(mean).append(sep).append(thirdQ).append(sep).append(max).append(sep).append(sd);
+        return sb.toString();
+    }
 }
